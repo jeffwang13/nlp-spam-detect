fe6e5bdfedfb0bb42a7d3717086580bf2394ee5c
Jeffrey Wang <jeffrey868@gmail.com>
2019-05-09 21:12:31 +0800

    Pipe data into SVM and test model, output confusion matrix/

--- email_spam_detect.py
+++ email_spam_detect.py
@@ -8,6 +8,7 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix
 from sklearn import svm
+from sklearn.model_selection import train_test_split
 
 stemmer = stem.SnowballStemmer('english')
 stopwords = set(stopwords.words('english'))
@@ -75,33 +76,39 @@ def email_to_plain(email):
             return html_to_plain(part)
 
 
+# Load filenames
 ham_test_filenames = [name for name in sorted(os.listdir('input/ham_test')) if len(name) > 20]
 ham_train_filenames = [name for name in sorted(os.listdir('input/ham_training')) if len(name) > 20]
 spam_test_filenames = [name for name in sorted(os.listdir('input/spam_test')) if len(name) > 20]
 spam_train_filenames = [name for name in sorted(os.listdir('input/spam_training')) if len(name) > 20]
 
-
+# Load email files
 ham_test_emails = [load_email('input/ham_test', filename=name) for name in ham_test_filenames]
 ham_train_emails = [load_email('input/ham_training', filename=name) for name in ham_train_filenames]
 spam_test_emails = [load_email('input/spam_test', filename=name) for name in spam_test_filenames]
 spam_train_emails = [load_email('input/spam_training', filename=name) for name in spam_train_filenames]
 
+# Sanitize email data, [text, (0: ham, 1: spam)]
+ham_train_emails = [[sanitize_email(email_to_plain(email)), 0] for email in ham_train_emails]
+ham_test_emails = [[sanitize_email(email_to_plain(email)), 0] for email in ham_test_emails]
+spam_train_emails = [[sanitize_email(email_to_plain(email)), 1] for email in spam_train_emails]
+spam_test_emails = [[sanitize_email(email_to_plain(email)), 1] for email in spam_test_emails]
+all_objects = ham_train_emails + spam_train_emails + ham_test_emails + spam_test_emails
 
-ham_train_emails = [sanitize_email(email_to_plain(email)) for email in ham_train_emails]
-ham_test_emails = [sanitize_email(email_to_plain(email)) for email in ham_test_emails]
-spam_train_emails = [sanitize_email(email_to_plain(email)) for email in spam_train_emails]
-spam_test_emails = [sanitize_email(email_to_plain(email)) for email in spam_test_emails]
+emails = [obj[0] for obj in all_objects]
+labels = [obj[1] for obj in all_objects]
+
+# train test split
+train_emails, test_emails, train_labels, test_labels = train_test_split(emails, labels, test_size = 0.1, random_state = 1)
 
 vectorizer = TfidfVectorizer()
 
-ham_train_emails = vectorizer.fit_transform(ham_train_emails)
-ham_test_emails = vectorizer.fit_transform(ham_test_emails)
-spam_train_emails = vectorizer.fit_transform(spam_train_emails)
-spam_test_emails = vectorizer.fit_transform(spam_test_emails)
+train_emails = vectorizer.fit_transform(train_emails)
 
 svm = svm.SVC(C=1000)
-svm.fit(ham_train_emails, spam_train_emails)
+svm.fit(train_emails, train_labels)
 
-X_test = vectorizer.transform(ham_test_emails)
-y_pred = svm.predict(X_test)
-print(confusion_matrix(spam_test_emails, y_pred))
+test_emails = vectorizer.transform(test_emails)
+y_pred = svm.predict(test_emails)
+
+print(confusion_matrix(test_labels, y_pred))
